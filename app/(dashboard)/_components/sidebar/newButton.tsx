efca9d481dd4fdc58bb3b1e59121896747d50ec7
ckwong <ckwong@esrisingapore.com.sg>
2024-02-09 12:32:36 +0800

    Added hint component

--- app/(dashboard)/_components/sidebar/newButton.tsx
+++ app/(dashboard)/_components/sidebar/newButton.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Hint } from "@/components/hint";
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import { CreateOrganization } from "@clerk/nextjs";
 import { Plus } from "lucide-react";
@@ -9,9 +10,16 @@ export function NewButton() {
     <Dialog>
       <DialogTrigger>
         <div className="aspect-square">
-          <button className="bg-white/25 h-full w-full rounded-md flex items-center justify-center opacity-60 hover:opacity-100 transition">
-            <Plus className="text-white" />
-          </button>
+          <Hint
+            label="Create organization"
+            side="right"
+            align="start"
+            sideOffset={18}
+          >
+            <button className="bg-white/25 h-full w-full rounded-md flex items-center justify-center opacity-60 hover:opacity-100 transition">
+              <Plus className="text-white" />
+            </button>
+          </Hint>
         </div>
       </DialogTrigger>
       <DialogContent className="p-0 bg-transparent border-none max-w-[480px]">
