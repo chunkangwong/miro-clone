a3596128783d92bbe328792c85ddd344e2988ac2
ckwong <ckwong@esrisingapore.com.sg>
2024-02-10 01:28:51 +0800

    Added rename feature

--- components/modals/renameModal.tsx
+++ components/modals/renameModal.tsx
@@ -2,21 +2,67 @@
 
 import {
   Dialog,
-  DialogTitle,
+  DialogClose,
   DialogContent,
+  DialogDescription,
+  DialogFooter,
   DialogHeader,
 } from "@/components/ui/dialog";
+import { api } from "@/convex/_generated/api";
+import { useApiMutation } from "@/hooks/useApiMutation";
 import { useRenameModal } from "@/store/useRenameModal";
-import { DialogDescription } from "@radix-ui/react-dialog";
+import { useEffect, useState } from "react";
+import { toast } from "sonner";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import { Id } from "@/convex/_generated/dataModel";
 
 export const RenameModal = () => {
+  const { mutate: update, isLoading } = useApiMutation(api.board.update);
+
   const { isOpen, onClose, initialValues } = useRenameModal();
+
+  const [title, setTitle] = useState(initialValues.title);
+
+  useEffect(() => {
+    setTitle(initialValues.title);
+  }, [initialValues.title]);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    update({ id: initialValues.id as Id<"boards">, title })
+      .then(() => {
+        toast.success("Board title updated");
+      })
+      .catch(() => toast.error("Failed to update board title"))
+      .finally(onClose);
+  };
 
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
       <DialogContent>
         <DialogHeader>Edit board title</DialogHeader>
         <DialogDescription>Enter a new title for this board</DialogDescription>
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <Input
+            disabled={false}
+            required
+            maxLength={60}
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            placeholder="Board title"
+          />
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button type="button" variant="outline">
+                Cancel
+              </Button>
+            </DialogClose>
+            <Button disabled={isLoading} type="submit">
+              Save
+            </Button>
+          </DialogFooter>
+        </form>
       </DialogContent>
     </Dialog>
   );
