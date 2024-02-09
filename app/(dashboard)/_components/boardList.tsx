1f7f25aec7c9864b62004066dbf0ee30a57b3c1a
ckwong <ckwong@esrisingapore.com.sg>
2024-02-10 02:07:37 +0800

    Added filtering by search and favourites

--- app/(dashboard)/_components/boardList.tsx
+++ app/(dashboard)/_components/boardList.tsx
@@ -17,7 +17,7 @@ interface BoardListProps {
 }
 
 export function BoardList({ orgId, query }: BoardListProps) {
-  const data = useQuery(api.boards.get, { orgId });
+  const data = useQuery(api.boards.get, { orgId, ...query });
 
   if (data === undefined) {
     return (
--- convex/boards.ts
+++ convex/boards.ts
@@ -1,9 +1,12 @@
 import { v } from "convex/values";
 import { query } from "./_generated/server";
+import { getAllOrThrow } from "convex-helpers/server/relationships";
 
 export const get = query({
   args: {
     orgId: v.string(),
+    search: v.optional(v.string()),
+    favourites: v.optional(v.string()),
   },
   handler: async (ctx, args) => {
     const identity = await ctx.auth.getUserIdentity();
@@ -12,11 +15,43 @@ export const get = query({
       throw new Error("Unauthorized");
     }
 
-    const boards = await ctx.db
-      .query("boards")
-      .withIndex("by_org", (q) => q.eq("orgId", args.orgId))
-      .order("desc")
-      .collect();
+    if (args.favourites) {
+      const favouriteBoards = await ctx.db
+        .query("userFavourites")
+        .withIndex("by_user_org", (q) =>
+          q.eq("userId", identity.subject).eq("orgId", args.orgId)
+        )
+        .order("desc")
+        .collect();
+
+      const ids = favouriteBoards.map((favourite) => favourite.boardId);
+
+      const boards = await getAllOrThrow(ctx.db, ids);
+
+      return boards.map((board) => ({
+        ...board,
+        isFavourite: true,
+      }));
+    }
+
+    const title = args.search as string;
+
+    let boards = [];
+
+    if (title) {
+      boards = await ctx.db
+        .query("boards")
+        .withSearchIndex("search_title", (q) =>
+          q.search("title", title).eq("orgId", args.orgId)
+        )
+        .collect();
+    } else {
+      boards = await ctx.db
+        .query("boards")
+        .withIndex("by_org", (q) => q.eq("orgId", args.orgId))
+        .order("desc")
+        .collect();
+    }
 
     const boardsWithFavouriteRelation = boards.map((board) => {
       return ctx.db
