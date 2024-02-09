6c75e391ccb038eadf75bf53501e71636ae1a3a9
ckwong <ckwong@esrisingapore.com.sg>
2024-02-09 14:06:01 +0800

    Added emptyOrg

---
+++ app/(dashboard)/_components/emptyOrg.tsx
@@ -0,0 +1,26 @@
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { CreateOrganization } from "@clerk/nextjs";
+import Image from "next/image";
+
+export function EmptyOrg() {
+  return (
+    <div className="h-full flex flex-col items-center justify-center">
+      <Image src="/elements.svg" alt="Empty" height={200} width={200} />
+      <h2 className="text-2xl font-semibold mt-6">Welcome to Board</h2>
+      <p className="text-muted-foreground text-sm mt-2">
+        Create an organization to get started
+      </p>
+      <div className="mt-6">
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button size="lg">Create organization</Button>
+          </DialogTrigger>
+          <DialogContent className="p-0 bg-transparent border-none max-w-[480px]">
+            <CreateOrganization />
+          </DialogContent>
+        </Dialog>
+      </div>
+    </div>
+  );
+}
--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,3 +1,14 @@
+"use client";
+
+import { useOrganization } from "@clerk/nextjs";
+import { EmptyOrg } from "./_components/emptyOrg";
+
 export default function DashboardPage() {
-  return <div>Dashboard Root Page</div>;
+  const { organization } = useOrganization();
+
+  return (
+    <div className="flex-1 h-[calc(100%-80px)] p-6">
+      {!organization ? <EmptyOrg /> : <p>Board List</p>}
+    </div>
+  );
 }
