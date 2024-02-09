45a3d5dd01c0af2b565ce00aaec941112a5f1d4b
ckwong <ckwong@esrisingapore.com.sg>
2024-02-09 13:38:45 +0800

    Added OrganizationSwitcher to orgSidebar

--- app/(dashboard)/_components/orgSidebar.tsx
+++ app/(dashboard)/_components/orgSidebar.tsx
@@ -1,9 +1,86 @@
 "use client";
 
+import Link from "next/link";
+import Image from "next/image";
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { OrganizationSwitcher } from "@clerk/nextjs";
+import { LayoutDashboard, Star } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { useSearchParams } from "next/navigation";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
+
 export function OrgSidebar() {
+  const searchParams = useSearchParams();
+
+  const favourites = searchParams.get("favourites");
+
   return (
-    <div className="hidden lg:flex flex-col space-y-6 w-[206px] pl-5 bg-red-500">
-      Org Sidebar
+    <div className="hidden lg:flex flex-col space-y-6 w-[206px] pl-5 pt-5">
+      <Link href="/">
+        <div className="flex items-center gap-x-2">
+          <Image src="/logo.svg" alt="Logo" height={60} width={60} />
+          <span className={cn("font-semibold text-2xl", font.className)}>
+            Board
+          </span>
+        </div>
+      </Link>
+      <OrganizationSwitcher
+        hidePersonal
+        appearance={{
+          elements: {
+            rootBox: {
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              width: "100%",
+            },
+            organizationSwitcherTrigger: {
+              padding: "6px",
+              width: "100%",
+              borderRadius: "6px",
+              border: "1px solid #E5E7EB",
+              justifyContent: "space-between",
+              backgroundColor: "white",
+            },
+          },
+        }}
+      />
+      <div className="space-y-1 w-full">
+        <Button
+          variant="ghost"
+          asChild
+          size="lg"
+          className="font-normal justify-start px-2 w-full"
+        >
+          <Link href="/">
+            <LayoutDashboard className="h-4 w-4 mr-2" />
+            Team Boards
+          </Link>
+        </Button>
+        <Button
+          variant={favourites ? "secondary" : "ghost"}
+          asChild
+          size="lg"
+          className="font-normal justify-start px-2 w-full"
+        >
+          <Link
+            href={{
+              pathname: "/",
+              query: {
+                favourites: "true",
+              },
+            }}
+          >
+            <Star className="h-4 w-4 mr-2" />
+            Favourite Boards
+          </Link>
+        </Button>
+      </div>
     </div>
   );
 }
