dd9e34d85bd6dddd407a8b69a0e51d93b01449c8
ckwong <ckwong@esrisingapore.com.sg>
2024-02-11 15:36:33 +0800

    Replaced deprecated toArray with toImmutable

--- app/boards/[boardId]/_components/selectionTools.tsx
+++ app/boards/[boardId]/_components/selectionTools.tsx
@@ -28,7 +28,7 @@ export const SelectionTools = memo(
 
         const indices: number[] = [];
 
-        const arr = liveLayerIds.toArray();
+        const arr = liveLayerIds.toImmutable();
 
         for (let i = 0; i < arr.length; i++) {
           if (selection.includes(arr[i])) {
@@ -49,7 +49,7 @@ export const SelectionTools = memo(
 
         const indices: number[] = [];
 
-        const arr = liveLayerIds.toArray();
+        const arr = liveLayerIds.toImmutable();
 
         for (let i = 0; i < arr.length; i++) {
           if (selection.includes(arr[i])) {
