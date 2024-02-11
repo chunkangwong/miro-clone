5ae907b4267cd1074910e576ad387392aae307e8
ckwong <ckwong@esrisingapore.com.sg>
2024-02-11 20:41:40 +0800

    Fixed button in button nesting error

--- app/(dashboard)/_components/sidebar/newButton.tsx
+++ app/(dashboard)/_components/sidebar/newButton.tsx
@@ -16,9 +16,9 @@ export function NewButton() {
             align="start"
             sideOffset={18}
           >
-            <button className="bg-white/25 h-full w-full rounded-md flex items-center justify-center opacity-60 hover:opacity-100 transition">
+            <div className="bg-white/25 h-full w-full rounded-md flex items-center justify-center opacity-60 hover:opacity-100 transition">
               <Plus className="text-white" />
-            </button>
+            </div>
           </Hint>
         </div>
       </DialogTrigger>
