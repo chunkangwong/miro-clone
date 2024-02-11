f5bc36389b70ecd75b7ffa48e2dd83d381ac268e
ckwong <ckwong@esrisingapore.com.sg>
2024-02-11 13:47:10 +0800

    Added selection net preview

--- app/boards/[boardId]/_components/canvas.tsx
+++ app/boards/[boardId]/_components/canvas.tsx
@@ -345,6 +345,16 @@ export const Canvas = ({ boardId }: CanvasProps) => {
           <SelectionBox
             onResizeHandlePointerDown={handleResizeHandlePointerDown}
           />
+          {canvasState.mode === CanvasMode.SelectionNet &&
+            canvasState.current && (
+              <rect
+                className="fill-blue-500/5 stroke-blue-500 stroke-1"
+                x={Math.min(canvasState.origin.x, canvasState.current.x)}
+                y={Math.min(canvasState.origin.y, canvasState.current.y)}
+                width={Math.abs(canvasState.origin.x - canvasState.current.x)}
+                height={Math.abs(canvasState.origin.y - canvasState.current.y)}
+              />
+            )}
           <CursorPresence />
         </g>
       </svg>
